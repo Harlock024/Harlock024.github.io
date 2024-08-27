eee00ff362b9693d4b4732b18ed5e620250173e4
harlock024 <hadith024@gmail.com>
2024-08-26 22:33:14 -0600

    my cv v1

--- src/app/Components/main/main.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-main",
-  standalone: true,
-  imports: [],
-  templateUrl: "./main.component.html",
-})
-export class MainComponent {}
