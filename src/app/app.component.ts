d09b7827b2a30ec2e8d27ce7ff89d2da110c14ab
harlock024 <hadith024@gmail.com>
2024-08-26 22:22:25 -0600

    add header v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from "@angular/core";
 // import { RouterOutlet } from "@angular/router";
-import { FooterComponent } from "./Components/footer/footer.component";
+
 import { HeaderComponent } from "./Components/header/header.component";
-import { MainComponent } from "./Components/main/main.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HeaderComponent, MainComponent, FooterComponent],
+  imports: [HeaderComponent],
   templateUrl: "./app.component.html",
 })
 export class AppComponent {
