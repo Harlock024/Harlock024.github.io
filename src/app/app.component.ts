c0ef28b9e8d2fd74c7506e39d3666122540d550c
harlock024 <hadith024@gmail.com>
2024-09-03 11:38:48 -0600

    feat: education component v1 #5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from "@angular/core";
 
 import { HeaderComponent } from "./Components/header/header.component";
 import { WorkExperienceComponent } from "./Components/work-experience/work-experience.component";
+import { EducationComponent } from "./Components/education/education.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HeaderComponent, WorkExperienceComponent],
+  imports: [HeaderComponent, WorkExperienceComponent, EducationComponent],
   templateUrl: "./app.component.html",
 })
 export class AppComponent {
