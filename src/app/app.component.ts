8f5675b3b26b37846a16f6b4290db0f2ec14f639
Hadith024 <144747775+Harlock024@users.noreply.github.com>
2024-09-20 20:43:00 -0600

    Skills (#40)
    * feat: skills component added #6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,8 @@ import { Component } from "@angular/core";
 import { HeaderComponent } from "./Components/header/header.component";
 import { WorkExperienceComponent } from "./Components/work-experience/work-experience.component";
 import { EducationComponent } from "./Components/education/education.component";
-import { ArchivementsComponent } from "./Components/archivements/archivements.component";
 import { SkillComponent } from "./Components/skill/skill.component";
-
+import { ArchivementsComponent } from "./Components/archivements/archivements.component";
 
 @Component({
   selector: "app-root",
