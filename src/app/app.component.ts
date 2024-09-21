a10830142ece6bb88dfa5830ec6e0e21f0075444
Hadith024 <144747775+Harlock024@users.noreply.github.com>
2024-09-20 20:53:02 -0600

    Hotfix (#41)
    * fix: merge components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from "@angular/core";
 // import { RouterOutlet } from "@angular/router";
-
 import { HeaderComponent } from "./Components/header/header.component";
 import { WorkExperienceComponent } from "./Components/work-experience/work-experience.component";
 import { EducationComponent } from "./Components/education/education.component";
-import { SkillComponent } from "./Components/skill/skill.component";
 import { ArchivementsComponent } from "./Components/archivements/archivements.component";
+import { SkillComponent } from "./Components/skill/skill.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
+
   imports: [
     HeaderComponent,
     WorkExperienceComponent,
