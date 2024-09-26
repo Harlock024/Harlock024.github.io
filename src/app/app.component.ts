9866083652ce12e75a0ca161956f6db22d28e17e
Hadith024 <144747775+Harlock024@users.noreply.github.com>
2024-09-26 01:16:42 -0600

    Release (#48)
    # LAST VERSION OF MY CV PROJECT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from "@angular/core";
 // import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./Components/header/header.component";
+import { WorkExperienceComponent } from "./Components/work-experience/work-experience.component";
+import { EducationComponent } from "./Components/education/education.component";
+import { ArchivementsComponent } from "./Components/archivements/archivements.component";
+import { SkillComponent } from "./Components/skill/skill.component";
+import { LenguagesComponent } from "./Components/lenguages/lenguages.component";
+import { InterestsComponent } from "./Components/interests/interests.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HeaderComponent],
+
+  imports: [
+    HeaderComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+    ArchivementsComponent,
+    SkillComponent,
+    LenguagesComponent,
+    InterestsComponent,
+  ],
   templateUrl: "./app.component.html",
 })
 export class AppComponent {
