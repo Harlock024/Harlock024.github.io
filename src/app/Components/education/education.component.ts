ae9be913afd666b1bd592282cbda48cf246cadc0
Hadith024 <144747775+Harlock024@users.noreply.github.com>
2024-09-26 08:56:31 -0600

    Release (#55)
    * Harlock024 patch 2 (#51)
    
    * cv project
    
    * cv project
    
    * cv project
    
    * add header v1
    
    * header v0.1
    
    * feat: header v1 #3
    
    * fetch(header): update About Me section (#3)
    
    * work experience
    
    * feat:work_experience component
    
    * feat:work_experience component
    
    * feat: karma config addded
    
    * try action
    
    * Create develop_CI_CD.yml
    
    * Harlock024 patch 1 update develop_ci_cd (#27)
    
    * Update develop_CI_CD.yml
    
    * Update develop_CI_CD.yml
    
    * fix type module (#28)
    
    * Harlock024 patch 1 (#34)
    
    
    build docker image
    
    * Education (#35)
    
    * education component added
    
    * Education (#36)
    
    * feat: education component v2 #5
    
    * Skills (#37)
    
    
    
    * feat: skills component added #6
    
    * Archivements (#39)
    
    
    * feat: archivement component added #9
    
    * Skills (#40)
    
    
    
    * feat: skills component added #6
    
    * Hotfix (#41)
    
    
    * fix: merge components
    
    * Languages (#42)
    
    
    * feat: languages component adedd #7
    
    * Interests (#43)
    
    
    
    * feat: interests component added #8
    
    * Update develop_CI_CD.yml (#44)
    
    
    * Update develop_CI_CD.yml
    
    * Update develop_CI_CD.yml
    
    * Update develop_CI_CD.yml
    
    * build updated

---
+++ src/app/Components/education/education.component.ts
@@ -0,0 +1,31 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "app-education",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./education.component.html",
+})
+export class EducationComponent {
+  Educations = [
+    {
+      school: "Universidad Veracruzana",
+      degree: "Licenciatura en Ingenieria de Software",
+      startDate: "Jan 2017",
+      endDate: "Present",
+    },
+    {
+      school: "Preparatoria Cobaev 06",
+      degree: "Bachillerato en Informatica",
+      startDate: "Jan 2019",
+      endDate: "Ago 2022",
+    },
+    {
+      school: "Secundaria Martiles de 1907",
+      degree: "Secundaria",
+      startDate: "Jan 2015",
+      endDate: "Ago 2018",
+    },
+  ];
+}
---
+++ src/app/Components/interests/interests.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-interests",
+  standalone: true,
+  imports: [],
+  templateUrl: "./interests.component.html",
+})
+export class InterestsComponent {}
