523fc822f19d578b6b38c50b4c6a94fefa81bd9c
Hadith024 <144747775+Harlock024@users.noreply.github.com>
2024-09-26 08:24:56 -0600

    Release (#52)
    * Harlock024 patch 2 (#51)
    
    * cv project
    
    * cv project
    
    * cv project
    
    * add header v1
    
    * header v0.1
    
    * feat: header v1 #3
    
    * fetch(header): update About Me section (#3)
    
    * work experience
    
    * feat:work_experience component
    
    * feat:work_experience component
    
    * feat: karma config addded
    
    * try action
    
    * Create develop_CI_CD.yml
    
    * Harlock024 patch 1 update develop_ci_cd (#27)
    
    * Update develop_CI_CD.yml
    
    * Update develop_CI_CD.yml
    
    * fix type module (#28)
    
    * Harlock024 patch 1 (#34)
    
    
    build docker image
    
    * Education (#35)
    
    * education component added
    
    * Education (#36)
    
    * feat: education component v2 #5
    
    * Skills (#37)
    
    
    
    * feat: skills component added #6
    
    * Archivements (#39)
    
    
    * feat: archivement component added #9
    
    * Skills (#40)
    
    
    
    * feat: skills component added #6
    
    * Hotfix (#41)
    
    
    * fix: merge components
    
    * Languages (#42)
    
    
    * feat: languages component adedd #7
    
    * Interests (#43)
    
    
    
    * feat: interests component added #8
    
    * Update develop_CI_CD.yml (#44)
    
    
    * Update develop_CI_CD.yml
    
    * Update develop_CI_CD.yml
    
    * Update develop_CI_CD.yml

---
+++ src/app/Components/lenguages/lenguages.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-lenguages",
+  standalone: true,
+  imports: [],
+  templateUrl: "./lenguages.component.html",
+})
+export class LenguagesComponent {}
